a548b65b86c08a1ee67f17811c10e7cc631daa2d
icedwater <icedwater@gmail.com>
2021-03-07 00:45:00 +0800

    Added basic_ann.py with working imports.

---
+++ basic_ann.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import numpy
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import optimizers
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# read in housing.csv using pandas - from where?
+dataframe = pd.read_csv("housing.csv", sep=',', header=0)
+dataset = dataframe.values

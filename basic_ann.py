016118c8e1b728cdaa399b6e7603d0960ee01c1e
icedwater <icedwater@gmail.com>
2021-03-07 11:26:30 +0800

    Added first working cut of ANN model.
    Used 1 dense layer with sigmoid activation as in the book.
    However, the MSE seems much better (~20 rather than ~160).
    
    I ran Python 3.6.9 and tf 2.4.1, so your mileage may vary.

--- basic_ann.py
+++ basic_ann.py
@@ -14,3 +14,30 @@ from sklearn.pipeline import Pipeline
 # read in housing.csv using pandas (github.com/PacktPublishing/Mobile-Artificial-Intelligence-Projects/)
 dataframe = pd.read_csv("housing.csv", sep=',', header=0)
 dataset = dataframe.values
+
+# print(dataframe.head()) ## uncomment to show top of dataframe
+
+# book uses a 70/30 split
+features = dataset[:, 0:7]
+target = dataset[:, 7]
+
+## ANN bit starts here ##
+
+def simple_shallow_seq_net():
+    """
+    Create a sequential ANN.
+    """
+    model = Sequential()
+    model.add(Dense(7, input_dim=7, kernel_initializer="normal", activation="sigmoid"))
+    model.add(Dense(1, kernel_initializer="normal"))
+    sgd = optimizers.SGD(lr=0.01)
+    model.compile(loss="mean_squared_error", optimizer=sgd)
+    return model
+
+## validation settings
+seed = 5    ## fixed for reproducibility, do change it
+numpy.random.seed(seed)
+kfold = KFold(n_splits=10, random_state=seed, shuffle=True)
+estimator = KerasRegressor(build_fn=simple_shallow_seq_net, epochs=100, batch_size=50, verbose=0)
+results = cross_val_score(estimator, features, target, cv=kfold)
+print(f"simple_shallow_seq_net model: {results.std():5.3} MSE")
